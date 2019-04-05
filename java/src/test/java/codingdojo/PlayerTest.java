6124bbdb685076f9fc35b1fea8d9f35938856275
Samuel Ytterbrink <say@praqma.net>
2019-04-05 14:22:04 +0200

    Add a example test that is not using mokito.

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -2,17 +2,38 @@ package codingdojo;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
     @Test
-    public void damageCalculations() {
+    public void damageCalculationsWithMocks() {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
 
+        Damage damage = new Player(inventory, stats).calculateDamage(target);
+        assertEquals(10, damage.getAmount());
+    }
+
+    @Test
+    public void damageCalculations() {
+        Inventory inventory = new Inventory(null) {
+
+        };
+        Stats stats = new Stats(10) {
+
+        };
+        SimpleEnemy target = new SimpleEnemy(null, null) {
+            @Override
+            List<Buff> getBuffs() {
+                return Collections.emptyList();
+            }
+        };
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(10, damage.getAmount());
     }
