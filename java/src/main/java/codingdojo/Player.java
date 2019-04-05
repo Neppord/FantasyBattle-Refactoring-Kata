d378e7f3467ebf755d6a624147976e25f61bf391
Samuel Ytterbrink <say@praqma.net>
2019-04-05 14:03:11 +0200

    Extract methods to make them easier top move

--- java/src/main/java/codingdojo/Player.java
+++ java/src/main/java/codingdojo/Player.java
@@ -4,37 +4,18 @@ package codingdojo;
 class Player extends Target {
     // TODO: add player buffs
 
+    private Inventory inventory;
+    private Stats stats;
+
     Player(Inventory inventory, Stats stats) {
         this.inventory = inventory;
         this.stats = stats;
     }
 
-    private Inventory inventory;
-    private Stats stats;
-
     Damage attack (Target other) {
-        Equipment equipment = this.inventory.getEquipment();
-        Item leftHand = equipment.getLeftHand();
-        Item rightHand = equipment.getRightHand();
-        Item head = equipment.getHead();
-        Item feet = equipment.getFeet();
-        Item chest = equipment.getChest();
-        int baseDamage = (
-            leftHand.getBaseDamage() +
-            rightHand.getBaseDamage() +
-            head.getBaseDamage() +
-            feet.getBaseDamage() +
-            chest.getBaseDamage()
-        );
-        float strengthModifier = stats.getStrength() * 0.1f;
-        int totalDamage = Math.round(baseDamage * (
-            strengthModifier +
-            leftHand.getDamageModifier() +
-            rightHand.getDamageModifier() +
-            head.getDamageModifier() +
-            feet.getDamageModifier() +
-            chest.getDamageModifier()
-        ));
+        int baseDamage = getBaseDamage();
+        float damageModifier = getDamageModifier();
+        int totalDamage = Math.round(baseDamage * damageModifier);
 
         if (other instanceof Player) {
             // TODO: Not implemented yet
@@ -55,5 +36,35 @@ class Player extends Target {
             return new Damage(Math.max(0, totalDamage - soak));
         }
         return new Damage(0);
+    }
+
+    private float getDamageModifier() {
+        Equipment equipment = this.inventory.getEquipment();
+        Item leftHand = equipment.getLeftHand();
+        Item rightHand = equipment.getRightHand();
+        Item head = equipment.getHead();
+        Item feet = equipment.getFeet();
+        Item chest = equipment.getChest();
+        float strengthModifier = stats.getStrength() * 0.1f;
+        return strengthModifier +
+            leftHand.getDamageModifier() +
+            rightHand.getDamageModifier() +
+            head.getDamageModifier() +
+            feet.getDamageModifier() +
+            chest.getDamageModifier();
+    }
+
+    private int getBaseDamage() {
+        Equipment equipment = this.inventory.getEquipment();
+        Item leftHand = equipment.getLeftHand();
+        Item rightHand = equipment.getRightHand();
+        Item head = equipment.getHead();
+        Item feet = equipment.getFeet();
+        Item chest = equipment.getChest();
+        return leftHand.getBaseDamage() +
+        rightHand.getBaseDamage() +
+        head.getBaseDamage() +
+        feet.getBaseDamage() +
+        chest.getBaseDamage();
     }
 }
