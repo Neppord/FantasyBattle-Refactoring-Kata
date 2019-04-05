dc993a742e942af9b5c6244dddbb1e5bb79894b1
Emily Bache <emily.bache@praqma.com>
2019-04-05 16:25:48 +0200

    make the second test slightly simpler to begin with

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -3,14 +3,14 @@ package codingdojo;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
+    // choose this one if you are familiar with mocks
     @Ignore("Test is not finished yet")
     @Test
     public void damageCalculationsWithMocks() {
@@ -22,21 +22,13 @@ public class PlayerTest {
         assertEquals(10, damage.getAmount());
     }
 
+    // choose this one if you are not familiar with mocks
     @Ignore("Test is not finished yet")
     @Test
     public void damageCalculations() {
-        Inventory inventory = new Inventory(null) {
-
-        };
-        Stats stats = new Stats(10) {
-
-        };
-        SimpleEnemy target = new SimpleEnemy(null, null) {
-            @Override
-            List<Buff> getBuffs() {
-                return Collections.emptyList();
-            }
-        };
+        Inventory inventory = new Inventory(null);
+        Stats stats = new Stats(0);
+        SimpleEnemy target = new SimpleEnemy(null, null);
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(10, damage.getAmount());
     }
