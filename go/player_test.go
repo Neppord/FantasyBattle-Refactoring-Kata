a6a9bd022f1181d56de82b6584b2126481eadbb5
Peter Kofler <peter.kofler@code-cop.org>
2022-01-12 14:16:03 +0100

    Sample test.

---
+++ go/player_test.go
@@ -0,0 +1,30 @@
+package codingdojo
+
+import (
+	"testing"
+	// "github.com/stretchr/testify/assert"
+)
+
+// choose this one if you are familiar with mocks
+func TestDamageCalculationsWithMocks(t *testing.T) {
+	t.Skip("Test is not finished yet")
+	// inventory := mockInventory()
+	// stats := mockStats()
+	// target := mockSimpleEnemy()
+
+	// damage := MakePlayer(inventory, stats).CalculateDamage(target)
+
+	// assert.EqualValues(t, 10, damage.GetAmount())
+}
+
+// choose this one if you are not familiar with mocks
+func TestDamageCalculations(t *testing.T) {
+	t.Skip("Test is not finished yet")
+	// inventory := MakeInventory(/* TODO */)
+	// stats := MakeStats(0)
+	// target := MakeSimpleEnemy(/* TODO */, /* TODO */)
+
+	// damage := MakePlayer(inventory, stats).CalculateDamage(target)
+
+	// assert.EqualValues(t, 10, damage.GetAmount())
+}
