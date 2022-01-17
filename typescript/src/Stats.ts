2fe96ad15233261f73b9bf22eb0088e937e2f6cd
Peter Kofler <peter.kofler@code-cop.org>
2022-01-17 17:33:52 +0100

    Add missing TODO as in original code.

--- typescript/src/Stats.ts
+++ typescript/src/Stats.ts
@@ -1,8 +1,10 @@
 export class Stats {
+    // TODO add dexterity that will both help with soak and damage.
+    //  but half of what strength gives.
     public constructor(private _strength: number) {
     }
 
-    get strength(): number {
+    public get strength(): number {
         return this._strength;
     }
 }
