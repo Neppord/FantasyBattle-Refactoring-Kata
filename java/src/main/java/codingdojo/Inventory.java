75b08c407071c2e18fb4b65f96107f0e2278d344
neppord <neppord@gmail.com>
2022-05-02 12:55:32 +0200

    Fix final field complaint from intellij

--- java/src/main/java/codingdojo/Inventory.java
+++ java/src/main/java/codingdojo/Inventory.java
@@ -1,7 +1,7 @@
 package codingdojo;
 
 public class Inventory {
-    private Equipment equipment;
+    private final Equipment equipment;
 
     public Inventory(Equipment equipment) {
         this.equipment = equipment;
