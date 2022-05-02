cff7221c38e4163695cb5a513059dea7939116ab
neppord <neppord@gmail.com>
2022-05-02 13:57:23 +0200

    Remove done TODO

--- java/src/main/java/codingdojo/Stats.java
+++ java/src/main/java/codingdojo/Stats.java
@@ -1,8 +1,6 @@
 package codingdojo;
 
 public class Stats {
-    // TODO add dexterity that will both help with soak and damage.
-    //  but half of what strength gives.
     private final int strength;
     private final int dexterity;
 
